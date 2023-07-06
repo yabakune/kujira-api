834a73b799794f1e9896dfa80237ce570b204289
Sekkacchi <sekkacchi@gmail.com>
2023-07-06 07:49:52 -0400

    Initialize server.ts

---
+++ src/server.ts
@@ -0,0 +1,58 @@
+import express, {
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from "express";
+import compression from "compression";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
+
+dotenv.config();
+const app = express();
+
+app.use(cors()); //Sets CORS for all routes.
+app.use(helmet()); //Sets HTTP Headers to protect app from well-known web vulnerabilities.
+app.use(compression()); // Compresses all routes.
+
+// ↓↓↓ Rate limits to a max of 20 requests per minute. ↓↓↓ //
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    rateLimit({
+      windowMs: 1 * 60 * 1000, // one minute
+      max: 20,
+    })
+  );
+}
+
+app.use(express.json()); // Allows API to parse client payload.
+
+// ↓↓↓ Routes ↓↓↓
+enum RouteBases {
+  AUTH = "/api/v1/auth",
+  USERS = "/api/v1/users",
+  OVERVIEWS = "/api/v1/overviews",
+  LOGBOOKS = "/api/v1/logbooks",
+  ENTRIES = "/api/v1/entries",
+  PURCHASES = "/api/v1/purchases",
+  BUG_REPORTS = "/api/v1/bug-reports",
+}
+
+const errorFallbackMiddleware: ErrorRequestHandler = (
+  error: Error,
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  if (error) response.json({ error: error.message });
+};
+app.use(errorFallbackMiddleware);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(
+    `🚀 Success! CORS-enabled web server is running at https://localhost:${port}`
+  );
+});
