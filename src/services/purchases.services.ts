fbb11f6e2e677eaa306f4502a29aba6709f092f3
Sekkacchi <sekkacchi@gmail.com>
2023-07-11 11:43:10 -0400

    Add getPurchase() service.

--- src/services/purchases.services.ts
+++ src/services/purchases.services.ts
@@ -7,20 +7,18 @@ import * as Validators from "@/validators";
 
 const prisma = new PrismaClient();
 
-export async function fetchPurchases(response: Response) {
+export async function getPurchases(response: Response) {
   try {
     const purchases = await prisma.purchase.findMany({
       orderBy: { id: "asc" },
     });
 
-    return response
-      .status(Constants.HttpStatusCodes.OK)
-      .json(
-        Helpers.generateResponse({
-          body: "Fetched purchases!",
-          response: purchases,
-        })
-      );
+    return response.status(Constants.HttpStatusCodes.OK).json(
+      Helpers.generateResponse({
+        body: "Fetched purchases!",
+        response: purchases,
+      })
+    );
   } catch (error) {
     console.error(error);
     return response
@@ -30,5 +28,27 @@ export async function fetchPurchases(response: Response) {
           body: "There was an error fetching purchases. Please refresh the page.",
         })
       );
+  }
+}
+
+export async function getPurchase(response: Response, purchaseId: number) {
+  try {
+    const purchase = await prisma.purchase.findUniqueOrThrow({
+      where: { id: purchaseId },
+    });
+
+    return response.status(Constants.HttpStatusCodes.OK).json(
+      Helpers.generateResponse({
+        body: "Fetched purchase!",
+        response: purchase,
+      })
+    );
+  } catch (error) {
+    console.error(error);
+    return response.status(Constants.HttpStatusCodes.NOT_FOUND).json(
+      Helpers.generateErrorResponse({
+        body: Constants.Errors.PURCHASE_DOES_NOT_EXIST,
+      })
+    );
   }
 }
